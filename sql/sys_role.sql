05651f2346d297fa998d3babacafa4bfd5e485d0
Administrator <1079648153@qq.com>
2019-06-21 07:31:03 +0800

    sql

---
+++ sql/sys_role.sql
@@ -0,0 +1,1 @@
+INSERT INTO `sys_role` (`id`, `role_name`, `comments`, `isDelete`, `opUser`, `create_time`, `update_time`, `uid`, `rid`) VALUES ('894e1550fed4d2fc387ed46951d3817a', '测试', 's2', NULL, NULL, NULL, NULL, NULL, NULL);
